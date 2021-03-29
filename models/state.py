019d8c5729fa216971d9dd3ad08d26f89b23637e
JulianArbini97 <1890@holbertonschool.com>
2021-03-29 22:56:13 +0000

    if getenv y esas cuestiones

--- models/state.py
+++ models/state.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
-from os import getenv
+import os
 import models
 from models.city import City
 
@@ -11,15 +11,19 @@ from models.city import City
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete")
 
-    @property
-    def cities(self):
-        """Return City list"""
-        lista = []
-        obj_dict = models.storage.all(City)
-        for key, value in obj_dict.items():
-            if value.state_id == self.id:
-                lista.append(obj_dict[key])
-        return lista
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """Return City list"""
+            lista = []
+            obj_dict = models.storage.all(City)
+            for key, value in obj_dict.items():
+                if value.state_id == self.id:
+                    lista.append(obj_dict[key])
+            return lista
