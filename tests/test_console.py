03cb76d7b9429a1e68bdf5322a54ecc4b4e6ac55
facu2279 <2279@holbertonschool.com>
2021-03-30 20:37:55 +0000

    fix

--- tests/test_console.py
+++ tests/test_console.py
@@ -46,24 +46,12 @@ class test_pep8aa(unittest.TestCase):
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
 
-    class TestFileStorage(unittest.TestCase):
-        """Test the FileStorage class"""
-        @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-        def test_all_returns_dict(self):
-            """Test that all returns a dictionaty"""
-            self.assertIs(type(models.storage.all()), dict)
-
-        @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-        def test_all_no_class(self):
-            """Test that all returns all rows when no class is passed"""
-
-        @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-        def test_new(self):
-            """test that new adds an object to the database"""
-
-        @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-        def test_save(self):
-            """Test that save properly saves objects to file.json"""
+    def test_dbs_func_docstrings(self):
+        """Test for the presence of docstrings in DBStorage methods"""
+        self.assertIsNot(func[1].__doc__, None,
+                         "{:s} method needs a docstring".format(func[0]))
+        self.assertTrue(len(func[1].__doc__) >= 1,
+                        "{:s} method needs a docstring".format(func[0]))
 """
 link test pep8
 https://pep8.readthedocs.io/en/release-1.7.x/advanced.html
