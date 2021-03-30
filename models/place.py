66834a5271d7f18a411438ca8b416e8cd519a1bf
JulianArbini97 <1890@holbertonschool.com>
2021-03-30 15:26:45 +0000

    10

--- models/place.py
+++ models/place.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """This is the place class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 import os
 import models
+
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -23,6 +34,8 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=True)
         reviews = relationship("Review", backref="place",
                                cascade="all, delete")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                 viewonly=False)
 
     else:
 
@@ -47,3 +60,15 @@ class Place(BaseModel, Base):
                 if v.place_id == self.id:
                     lista.append(recorrido[k])
             return new_list
+
+        @property
+        def amenities(self):
+            """ Amenity list """
+            return self.amenities
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """ list of amenities instances """
+            if type(obj) is Amenity:
+                lista = []
+                self.lista.append(obj.id)
