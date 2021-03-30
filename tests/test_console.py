e4a3734330b6a8674363a8ebb0c119ca30fda0a5
facu2279 <2279@holbertonschool.com>
2021-03-30 20:44:35 +0000

    fix

--- tests/test_console.py
+++ tests/test_console.py
@@ -45,7 +45,7 @@ class test_pep8aa(unittest.TestCase):
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
-                        
+
 """
 link test pep8
 https://pep8.readthedocs.io/en/release-1.7.x/advanced.html
