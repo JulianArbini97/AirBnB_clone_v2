2d21c6109be07487d1530e54a6c0dc29312804da
facu2279 <2279@holbertonschool.com>
2021-03-30 18:11:25 +0000

    Add test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 """ xxxddd """
-import unittest
-from models.base_model import BaseModel
-from models import storage
 
+import pep8
+import models
+import MySQLdb
+import unittest
 
 class test_db_storage(unittest.TestCase):
     """ xd """
+    
+def test_add(self):
+    """ xdxd """
+    engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                           .format(getenv("HBNB_MYSQL_USER"), getenv("HBNB_MYSQL_PWD"), getenv("HBNB_MYSQL_HOST"), getenv("HBNB_MYSQL_DB")), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cant1 = 0
+    cant2 = 0
+    for instance in session.query(State).order_by(State.id):
+        cant1 = cant1 + 1
+    new = State()
+    for instance in session.query(State).order_by(State.id):
+        cant2 = cant2 + 1
+    self.assertEqual(cant1 + 1, cant2)
