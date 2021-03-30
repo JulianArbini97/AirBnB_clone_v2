356f4b1522f4318eff4763d91ab5a72ef42e8c93
facu2279 <2279@holbertonschool.com>
2021-03-30 21:11:09 +0000

    xd

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""
-Contains the class TestConsoleDocs
-"""
+""" aaaa """
 
-import console
-import inspect
 import pep8
 import unittest
-HBNBCommand = console.HBNBCommand
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.db_storage import DBStorage
+from os import getenv
+from console import HBNBCommand
 
 
-class TestConsoleDocs(unittest.TestCase):
-    """Class for testing documentation of the console"""
+class test_pep8aa(unittest.TestCase):
+    """ testingggggggggggggggggggggggggg """
     def test_pep8_conformance_console(self):
         """Test that console.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
@@ -28,9 +34,9 @@ class TestConsoleDocs(unittest.TestCase):
 
     def test_console_module_docstring(self):
         """Test for the console.py module docstring"""
-        self.assertIsNot(console.__doc__, None,
+        self.assertIsNot(__import__("console").__doc__, None,
                          "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
+        self.assertTrue(len(__import__("console").__doc__) >= 1,
                         "console.py needs a docstring")
 
     def test_HBNBCommand_class_docstring(self):
