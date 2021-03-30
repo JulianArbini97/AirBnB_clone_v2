52111d14eb58d70d1c1d95fc0d98b60cb4f7c2f0
facu2279 <2279@holbertonschool.com>
2021-03-30 02:03:30 +0000

    Add test pep8

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """ aaaa """
 
+import pep8
 import unittest
-HBNBCommand = console.HBNBCommand
 
 
-class TestConsoleDocs(unittest.TestCase):
-    """ sdfsdf """
-
-    def test_HBNBCommand_class_docstring(self):
-        """Test for the HBNBCommand class docstring"""
-        self.assertIsNot(HBNBCommand.__doc__, None,
-                         "HBNBCommand class needs a docstring")
-        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
-                        "HBNBCommand class needs a docstring")
+class test_pep8aa(unittest.TestCase):
+    """ testingggggggggggggggggggggggggg """
+    def test_pep8_conformance_console(self):
+        """Test that console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
